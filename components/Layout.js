30b3df35e5655a95ebc17a6d6a21c8bd0ecadfdd
nordstein-agency <office@nordstein-agency.com>
2025-10-16 23:28:56 +0000

    h

--- components/Layout.js
+++ components/Layout.js
@@ -3,11 +3,13 @@ import Navbar from './Navbar'
 
 export default function Layout({ children }) {
   return (
-    <div className="relative min-h-screen bg-white">
+    <div className="relative min-h-screen">
       {/* Gradient oben */}
-      <div className="absolute top-0 left-0 w-full h-[400px] pointer-events-none z-0"
-           style={{ background: 'linear-gradient(to bottom, #451a3d, #ffffff)' }} />
-      
+      <div
+        className="absolute top-0 left-0 w-full h-[400px] z-0 pointer-events-none"
+        style={{ background: 'linear-gradient(to bottom, #451a3d, #ffffff)' }}
+      />
+
       <div className="relative z-10">
         <Navbar />
         <main>{children}</main>
