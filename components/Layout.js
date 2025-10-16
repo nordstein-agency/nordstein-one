be464decc0bdd46fef3ede0f9065d83fdc532f1f
nordstein-agency <office@nordstein-agency.com>
2025-10-16 21:59:38 +0000

    h

--- components/Layout.js
+++ components/Layout.js
@@ -3,12 +3,12 @@ import Navbar from './Navbar'
 
 export default function Layout({ children }) {
   return (
-    <div className="relative min-h-screen">
-      {/* Farbverlauf-Hintergrund */}
+    <div className="relative min-h-screen bg-white">
+      {/* Farbverlauf nur im oberen Bereich */}
       <div
-        className="fixed top-0 left-0 w-full h-screen z-0 pointer-events-none"
+        className="absolute top-0 left-0 w-full h-[400px] z-0 pointer-events-none"
         style={{
-          background: 'linear-gradient(to bottom, #451a3d 0%, #ffffff 50vh)',
+          background: 'linear-gradient(to bottom, #451a3d 0%, #ffffff 100%)',
         }}
       ></div>
 
