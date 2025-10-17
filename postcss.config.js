cf82b2a6e34cc6652c429e0a2abb28d45916985b
nordstein-agency <office@nordstein-agency.com>
2025-10-17 12:59:51 +0000

    h

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
+/** @type {import('postcss').ProcessOptions} */
 module.exports = {
   plugins: {
     tailwindcss: {},
-    autoprefixer: {},
-  },
+    autoprefixer: {}
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,10 @@
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        inter: ['"Inter Tight"', 'sans-serif'],
-      },
-      colors: {
-        nordsteinBeige: '#e6ded3',
-        nordsteinPurple: '#451a3d',
-      },
-    },
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 }
