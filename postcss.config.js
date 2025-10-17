236be506c49acf28facc23467f841d6e48e680ea
nordstein-agency <office@nordstein-agency.com>
2025-10-17 13:18:11 +0000

    h

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 /** @type {import('postcss').ProcessOptions} */
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {}
-  }
-}
+  plugins: [
+    require('@tailwindcss/postcss'),
+    require('autoprefixer'),
+  ],
+};
